e398e827a26aa1e6c4d4598d5d6d47ab1d8b27d0
Jay Lee <jaylee.possible@gmail.com>
2020-01-28 01:03:20 +0900

    update clinet default actions and reducers (redux)

--- generators/client/templates/src/redux/reducers/app.js
+++ generators/client/templates/src/redux/reducers/app.js
@@ -1,22 +1,40 @@
-import { UPDATE_APP_NAME, UPDATE_BASE_URL } from '../actions'
+import {
+  UPDATE_APP_INFO,
+  UPDATE_APP_NAME,
+  UPDATE_APP_VERSION,
+  UPDATE_BASE_URL
+} from '../actions'
 
 const INITIAL_STATE = {
   name: '<%= appName %>',
-  baseUrl: location.origin
+  baseUrl: location.origin,
+  version: 'v0.0.1'
 }
 
 export const app = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case UPDATE_APP_INFO: {
+      return {
+        ...state,
+        ...action.app
+      }
+    }
     case UPDATE_APP_NAME:
       return {
         ...state,
-        name: action.name
+        name: action.app.name
       }
 
     case UPDATE_BASE_URL:
       return {
         ...state,
-        baseUrl: action.baseUrl
+        baseUrl: action.app.baseUrl
+      }
+
+    case UPDATE_APP_VERSION:
+      return {
+        ...state,
+        version: action.app.version
       }
 
     default:
