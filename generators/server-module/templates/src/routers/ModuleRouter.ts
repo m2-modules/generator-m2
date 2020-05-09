32b95e4f83f035bd5cb9749022498a20bfba13d7
Jay Lee <jaylee.possible@gmail.com>
2020-05-09 11:51:25 +0900

    type specified for router of server-module

--- generators/server-module/templates/src/routers/ModuleRouter.ts
+++ generators/server-module/templates/src/routers/ModuleRouter.ts
@@ -1,25 +1,25 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import { <%= uccModuleName %>Controller } from '../controllers'
 
 const router = express.Router()
 const <%= lccModuleName %>Controller = new <%= uccModuleName %>Controller()
 
 /* */
-router.post('/', async (req, res) => {
+router.post('/', async (req: Request, res: Response) => {
   res.json(await <%= lccModuleName %>Controller.create(req.body))
 })
 
-router.get('/', async (req, res) => {
+router.get('/', async (req: Request, res: Response) => {
   res.json(await <%= lccModuleName %>Controller.find())
 })
 
-router.get('/:id', (req, res) => {})
+router.get('/:id', (req: Request, res: Response) => {})
 
-router.put('/', async (req, res) => {
+router.put('/', async (req: Request, res: Response) => {
   res.json(await <%= lccModuleName %>Controller.update(req.body))
 })
 
-router.put('/:id', async (req: any, res) => {
+router.put('/:id', async (req: Request, res: Response) => {
   res.json(
     await <%= lccModuleName %>Controller.update({
       id: req.params.id,
